4abd64141c7808501870605157a8065eb4d51b92
andrewkah <107668588+andrewkah@users.noreply.github.com>
2023-12-21 13:48:18 +0300

    Added CORS config
    The CORS configuration was added in the securityFilterChain.

--- src/main/java/com/example/groceries/security/config/SecurityConfig.java
+++ src/main/java/com/example/groceries/security/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.groceries.security.config;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,9 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import com.example.groceries.security.entity.Role;
 import com.example.groceries.security.service.UserService;
+import com.example.groceries.security.service.impl.AuthServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,9 +35,19 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
     private final UserService userService;
 
+    Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        logger.info("Cors configuration has started");
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("https://d0f2-102-134-149-145.ngrok.io"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) 
                 .authorizeHttpRequests(request -> request
                                 .requestMatchers("/api/v1/auth/**", "/api/v1/category/**").permitAll() 
